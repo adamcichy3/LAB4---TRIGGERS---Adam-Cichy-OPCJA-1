caac6c24ee2eaf8e01476a759a470ef6cd991764
Adam Cichy <adam.cichyy3@gmail.com>
2018-05-06 09:03:37 +0200

    Cały projekt

---
+++ Triggers/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/BehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class BehaviourScript : MonoBehaviour {
+
+    public GameObject LightG;
+    // Use this for initialization
+    void Start () {
+       
+}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.GetComponent<Collider>().CompareTag("Player"))
+		{
+			LightG.GetComponent<Light>().color = Color.red;
+			//AudioSource sound = other.GetComponent<AudioSource>();
+			//sound.Play();
+		}
+	}
+}
